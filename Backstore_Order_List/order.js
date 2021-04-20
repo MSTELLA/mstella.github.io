ea649b89b4b2ca2ac5429b587a3608955ebee6b3
kyumiko <74166251+kyumiko@users.noreply.github.com>
2021-04-20 17:10:18 -0400

    Update order.js

--- Backstore_Order_List/order.js
+++ Backstore_Order_List/order.js
@@ -109,6 +109,7 @@ window.onload = function() {
     var nameValueArray = document.cookie.split("&");
     for (let i = 0; i < nameValueArray.length; i++){
       document.getElementById('item'+(i+1)).value = nameValueArray[i];
+      window.alert(i);
     }
     /*document.getElementById("item1").value = nameValueArray[0];
     document.getElementById("item2").value = nameValueArray[1];
