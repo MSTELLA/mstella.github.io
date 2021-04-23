5e22dbad12343b271343a60859de59abfdd2d3ad
guidote <74155514+guidote@users.noreply.github.com>
2021-04-23 12:10:23 -0400

    Update addUser.php

--- BackstoreUser/addUser.php
+++ BackstoreUser/addUser.php
@@ -1,24 +1,40 @@
 <?php
-if ( isset( $_GET['submit'] ) ) { 
-    $firstname = $_GET['fName']; 
-    $lastname = $_GET['lName'];     
-    $email = $_GET['email'];   
-    $phonenumber = $_GET['phoneNum'];     
-    $birthday = $_GET['bDay'];
-    $cardnumber = $_GET['card'];
+$productName=$_REQUEST["productName"];
+$brand=$_REQUEST["brand"];
+$price=$_REQUEST["price"];
+$priceQuantity=$_REQUEST["priceQuantity"];
+$description=$_REQUEST["description"];
+/*$address=$_REQUEST["address"];
+$postal=$_REQUEST["postal"];
+$email=$_REQUEST["email"];
+$mobile=$_REQUEST["mobile"];
+$membership=$_REQUEST["membership"];
+*/
+require_once('MYDB.php');
+$pdo= db_connect();
 
-    $simplexml= new SimpleXMLElement('<users><users/>');
-    
-    $user = $users->addChild('user');
-  
-    $user->addChild('firstName', $firstname);
-    $user->addChild('lastName', $lastname);
-    $user->addChild('Email', $email);
-    $user->addChild('phoneNumber', $phonenumber);
-    $user->addChild('birthday', $birthday);
-    $user->addChild('cardNumber', $cardnumber);
-  
+try{
+    $pdo->beginTransaction();
+    $sql="insert into product(productName, brand, price, priceQuantity, description)
+          values(?,?,?,?,?)";
+    $stmh=$pdo->prepare($sql);
+    $stmh->bindValue(1, $productName,PDO::PARAM_STR);
+    $stmh->bindValue(2, $brand,PDO::PARAM_STR);
+    $stmh->bindValue(3, $price,PDO::PARAM_STR);
+    $stmh->bindValue(4, $priceQuantity,PDO::PARAM_STR);
+    $stmh->bindValue(5, $description,PDO::PARAM_STR);
+    /*$stmh->bindValue(6, $address,PDO::PARAM_STR);
+    $stmh->bindValue(7, $postal,PDO::PARAM_STR);
+    $stmh->bindValue(8, $email,PDO::PARAM_STR);
+    $stmh->bindValue(9, $mobile,PDO::PARAM_STR);
+    $stmh->bindValue(10, $membership,PDO::PARAM_STR);
+    */
+    $stmh->execute();
+    $pdo->commit();
+    print "You have successfully Edited/Added a product!";
+      
+ } catch (PDOException $Exception) {
+     $pdo->rollBack();
+     print "ERROR :" .$Exception->getMessage();
 
-    $simple_xml->asXML('users.xml');
 }
-?>
